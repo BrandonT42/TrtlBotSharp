bcb5a7ef5fea93395df5612bb600ee57272c8640
BrandonT42 <brandon.chutuk@gmail.com>
2018-11-27 12:21:57 -0800

    Fixed supply display to use proper coinunit value, and floored database migration utility values to ignore extraneous decimal places

--- TrtlBotSharp/Utilities.cs
+++ TrtlBotSharp/Utilities.cs
@@ -21,7 +21,7 @@ namespace TrtlBotSharp
             if (Result.Count < 1 || Result.ContainsKey("error")) return 0;
 
             // Return current supply
-            return (decimal)Result["block"]["alreadyGeneratedCoins"] / 100;
+            return (decimal)Result["block"]["alreadyGeneratedCoins"] / coinUnits;
         }
 
         // Gets the bot's wallet address
